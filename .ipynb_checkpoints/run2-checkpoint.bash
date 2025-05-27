31902ce43d9f0d91aca1f068e87a2a6ca0d0c0ee
Hanzhong Cao <hc936@r815u35n07.grace.ycrc.yale.edu>
2025-05-26 23:56:13 -0400

    regression1

---
+++ .ipynb_checkpoints/run2-checkpoint.bash
@@ -0,0 +1,14 @@
+nohup python run.py --pre_model_name erpp --embed_name tale --task_epoch 20 --device 1 &
+nohup python run.py --pre_model_name gru --embed_name tale --task_epoch 20 --device 0 &
+nohup python run.py --pre_model_name rnn --embed_name tale --task_epoch 20 --device 3 &
+python run.py --pre_model_name stlstm --embed_name tale --task_epoch 20 --device 2
+
+nohup python run.py --pre_model_name erpp --embed_name teaser --task_epoch 20 --device 1 &
+nohup python run.py --pre_model_name gru --embed_name teaser --task_epoch 20 --device 0 &
+nohup python run.py --pre_model_name rnn --embed_name teaser --task_epoch 20 --device 3 &
+python run.py --pre_model_name stlstm --embed_name teaser --task_epoch 20 --device 2
+
+nohup python run.py --pre_model_name erpp --embed_name poi2vec --task_epoch 20 --device 1 &
+nohup python run.py --pre_model_name gru --embed_name poi2vec --task_epoch 20 --device 0 &
+nohup python run.py --pre_model_name rnn --embed_name poi2vec --task_epoch 20 --device 3 &
+python run.py --pre_model_name stlstm --embed_name poi2vec --task_epoch 20 --device 2
